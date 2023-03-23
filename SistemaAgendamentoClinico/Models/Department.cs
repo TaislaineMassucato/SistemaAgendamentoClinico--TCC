39f7201317b130cc439bb4e8c2dad9ea76457b1a
Taislaine Massucato <massucato2@outlook.com>
2023-03-23 15:53:35 -0300

    Deleting Departament view and controller

--- SistemaAgendamentoClinico/Models/Department.cs
+++ SistemaAgendamentoClinico/Models/Department.cs
@@ -1,4 +1,9 @@
-﻿namespace SistemaAgendamentoClinico.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaAgendamentoClinico.Models.ViewModels
 {
     public class Department
     {
