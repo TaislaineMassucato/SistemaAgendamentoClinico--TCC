12d59a623b77f760bf14d86e1306099666e4f4de
Taislaine Massucato <massucato2@outlook.com>
2023-03-23 16:00:34 -0300

    CRUD Scaffolding

---
+++ SistemaAgendamentoClinico/Data/SistemaAgendamentoClinicoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaAgendamentoClinico.Models.ViewModels;
+
+namespace SistemaAgendamentoClinico.Data
+{
+    public class SistemaAgendamentoClinicoContext : DbContext
+    {
+        public SistemaAgendamentoClinicoContext (DbContextOptions<SistemaAgendamentoClinicoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaAgendamentoClinico.Models.ViewModels.Department> Department { get; set; }
+    }
+}
