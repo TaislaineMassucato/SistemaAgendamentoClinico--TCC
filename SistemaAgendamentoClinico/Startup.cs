a1cd62c6d24b717fd739617add6885757f365e72
Taislaine Massucato <massucato2@outlook.com>
2023-03-24 12:41:52 -0300

    MySql adaptation and first migration

--- SistemaAgendamentoClinico/Startup.cs
+++ SistemaAgendamentoClinico/Startup.cs
@@ -37,7 +37,7 @@ namespace SistemaAgendamentoClinico
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SistemaAgendamentoClinicoContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SistemaAgendamentoClinicoContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SistemaAgendamentoClinicoContext"),builder => builder.MigrationsAssembly("SistemaAgendamentoClinico")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
